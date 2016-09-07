36aae82c3561280b66af0fef11342721df80913e
Winer Bao <winer-bao@hotmail.com>
2016-09-07 11:03:19 +0200

    Basic Maven project
    Follow this file structure for this entire project. This is a default
    Maven project generated by Eclipse IDE.

---
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/App.java
@@ -0,0 +1,13 @@
+package nl.tudelft.bust_a_move20162017.bust_a_move_framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
