9256580d9c3ded6cb75c3a7d0f133d2857be4bfb
Winer Bao <winer-bao@hotmail.com>
2016-09-07 11:17:36 +0200

    Empty classes added
    Uses these templates to create our classes

---
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/arena/Arena.java
@@ -0,0 +1,5 @@
+package nl.tudelft.bust_a_move20162017.bust_a_move_framework.arena;
+
+public class Arena {
+
+}
---
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/bubble/Bubble.java
@@ -0,0 +1,5 @@
+package nl.tudelft.bust_a_move20162017.bust_a_move_framework.bubble;
+
+public class Bubble {
+
+}
