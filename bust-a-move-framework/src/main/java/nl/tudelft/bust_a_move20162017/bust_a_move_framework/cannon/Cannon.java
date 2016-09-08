ea8a83379290a4604b651afa91c4f8e327947edf
Maurice <Maurice@Maurice-PC>
2016-09-08 14:50:25 +0200

    Cannon and Game class, First version

--- bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/cannon/Cannon.java
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/cannon/Cannon.java
@@ -1,68 +1,133 @@
+/*
+ * File: cannon.java
+ * Class: Cannon
+ *
+ * Version: 0.0.1
+ * Date: September 8th, 2016
+ */
+
 package nl.tudelft.bust_a_move20162017.bust_a_move_framework.cannon;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * The Cannon class represents a cannon entity.
+ *
+ * @author Maurice Willemsen
+ */
 
 public class Cannon {
 
 	private int ANGLE;
 
-	private int CurrBubbleColour;
+	private int currBubbleColour;
 
-	private int NextBubbleColour;
-	
-	public int X;
-	
-	public int Y;
+	private int nextBubbleColour;
+
+	private int X;
+
+	private int Y;
+
+	private int SIZE;
+
+	private Color cannonColour;
+
+	/**
+	 * Creates Cannon instance
+	 *
+	 * @param X
+	 *            int value for starting x position
+	 * @param Y
+	 *            int value for starting y position
+	 * @param SIZE
+	 *            int value for cannons length
+	 * @param cannonColour
+	 *            Color value for cannons colour
+	 */
 
 	public void main() {
-		this.ANGLE = 0;
-		this.NextBubbleColour = getNextColour();
 		this.X = 320;
-		this.Y = 0;
-		this.loadNextBubble();	
-	}
+		this.Y = 20;
+		this.SIZE = 20;
+		this.cannonColour = new Color(0x000000);
 
-	//Bubble Checking
-	private void loadNextBubble() {
-		this.CurrBubbleColour = this.NextBubbleColour;
-		this.NextBubbleColour = this.getNextColour();
-	}
-	
-	private int getNextColour(){
-		int nextColor = 1; //Needs to refer to function outside the class
-		return nextColor;		
-	}
-
-	
-	//Fire
-	public void fire() {
+		this.ANGLE = 0;
+		this.nextBubbleColour = getNextColour();
 		this.loadNextBubble();
 	}
-	
-	
 
-	//Turn Cannon
+	/**
+	 * Load next bubble to current bubble and asks new colour.
+	 */
+
+	private void loadNextBubble() {
+		// TODO Needs to get colour ENUM
+		this.currBubbleColour = this.nextBubbleColour;
+		this.nextBubbleColour = this.getNextColour();
+	}
+
+	/**
+	 * Determines next colour
+	 */
+
+	private int getNextColour() {
+		// TODO Needs to refer to bubble checking function
+		int nextColor = 1;
+		return nextColor;
+	}
+
+	/**
+	 * create a new bubble object and loads next bubble
+	 */
+
+	public void fire() {
+		// TODO Create new Bubble object
+		this.loadNextBubble();
+
+	}
+
+	/**
+	 * updates the Cannon angle per inputstep
+	 */
 	public void stepUp() {
 		if (this.ANGLE <= 60) {
 			this.ANGLE++;
 		}
 	}
-	
+
+	/**
+	 * updates the Cannon angle per inputstep
+	 */
+
 	public void stepDown() {
 		if (this.ANGLE >= 60) {
 			this.ANGLE--;
 		}
 	}
-	
-	//Gets
-	public int getAngle(){
-		return this.ANGLE;
+
+	/**
+	 * draws the Cannon
+	 * 
+	 * @param g
+	 *            Java Graphics instance
+	 */
+
+	public void drawCannon(Graphics g) {
+		g.setColor(this.cannonColour);
+		// TODO Make a nicer cannon
+		g.drawLine(this.X, this.Y, (int) (this.X + Math.cos(this.ANGLE) * this.SIZE),
+				(int) (this.X + Math.sin(this.ANGLE) * this.SIZE));
 	}
-	
-	public int nextBubbleColor(){
-		return this.ANGLE;
+
+	/**
+	 * draws the Next Bubbles
+	 * 
+	 * @param g
+	 *            Java Graphics instance
+	 */
+
+	public void drawNextBubbles(Graphics g) {
+		// TODO Draw the current and next bubble in the bottom of the screen.
 	}
-	
-	public int currBubbleColor(){
-		return this.CurrBubbleColour;
-	}
-	
 }
--- bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/game/Game.java
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/game/Game.java
@@ -1,44 +1,148 @@
+/*
+ * File: game.java
+ * Class: Game
+ *
+ * Version: 0.0.1
+ * Date: September 8th, 2016
+ */
+
 package nl.tudelft.bust_a_move20162017.bust_a_move_framework.game;
 
 import nl.tudelft.bust_a_move20162017.bust_a_move_framework.cannon.Cannon;
 import nl.tudelft.bust_a_move20162017.bust_a_move_framework.arena.Arena;
+import nl.tudelft.bust_a_move20162017.bust_a_move_framework.player.Player;
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * The Game class represents a game entity.
+ *
+ * @author Maurice Willemsen
+ */
 
 public class Game {
 
-	public int WIDTH;
-	
-	public int HEIGHT;
-	
-	private int LEVEL;
-	
-	private int DIFFICULTY;
-	
-	private boolean IsPlaying;
-	
-	private Cannon cannon;	
-	
-	private Arena arena;	
+	private enum State {
+		START, PLAYING, FAILED, WON
+	}
 
-	public void main() {
-		this.LEVEL = 1;
-		this.DIFFICULTY = 1;
+	public int WIDTH;
+
+	public int HEIGHT;
+
+	private int LEVEL;
+
+	private int DIFFICULTY;
+
+	private Cannon cannon;
+
+	private Arena arena;
+
+	private Player player;
+
+	private State state;
+
+	private Color backGroundcolor;
+
+	private Graphics g;
+	
+	/**
+	 * Creates Game instance
+	 *
+	 * @param WIDTH
+	 *            int value for screen width
+	 * @param HEIGHT
+	 *            int value for screen height
+	 * @param backgroundColour
+	 *            Color value for background
+	 */
+
+	public void main(Graphics g) {
 		this.WIDTH = 640;
 		this.HEIGHT = 480;
-		this.IsPlaying = false;	
+		this.backGroundcolor = new Color(0xFFFFFF);
+		
+		this.LEVEL = 1;
+		this.DIFFICULTY = 1;
+
+		this.state = State.START;		
+		this.player = new Player();
+		this.g = g;
 	}
 	
-	public void startGame(){
-		this.IsPlaying = true;
-		cannon = new Cannon();  
-		arena = new Arena();
+	/**
+	 * Starts game, changes state to PLAYING, creates new arena and cannon instance
+	 */
+
+	private void startGame() {
+		this.state = State.PLAYING;
+		this.cannon = new Cannon();
+		this.arena = new Arena();
 	}
 	
-	public void startNewLevel(){
+	/**
+	 * Ends game, changes state to WON
+	 */
+
+	private void wonGame() {
+		this.state = State.WON;
+	}
+	
+	/**
+	 * Ends game, changes state to FAILED
+	 */
+
+	private void failedGame() {
+		this.state = State.FAILED;
+	}
+	
+	/**
+	 * Asks for new level and new start of game.
+	 */
+
+	private void startNewLevel() {
 		this.levelUp();
 		this.startGame();
 	}
 	
-	private void levelUp(){
+	/**
+	 * Level up
+	 */
+
+	private void levelUp() {
 		this.LEVEL++;
+	}
+	
+	/**
+	 * Drop all bubbles one line and creates new bubbles
+	 */
+
+	public void newLine() {
+		// TODO Drop all bubbles one line
+		// TODO Add random amount of random bubbles to top line.
+	}
+	
+	/**
+	 * Updates the game
+	 */
+
+	public void update() {
+		switch (this.state) {
+		case START:
+			// TODO Show startscreen
+			break;
+		case PLAYING:
+			cannon.drawCannon(this.g);
+			// TODO Show playscreen including arena and cannon
+			break;
+		case WON:
+			// TODO Show wonscreen including score, level, and button for next
+			// level
+			break;
+		case FAILED:
+			// TODO Show failedscreen including score, level, and button for
+			// restart level
+			break;
+		}
 	}
 }
