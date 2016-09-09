b8de0bbad635299303b0af49efc15d910180fcaf
Calvin Quan-Luan Nhieu <cqnhieu@uwaterloo.ca>
2016-09-09 14:33:28 +0200

    fix player package name

--- bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/player/PlayerModel.java
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/player/PlayerModel.java
@@ -6,7 +6,7 @@
  * Date: September 8th, 2016
  */
 
-package nl.tudelft.bust_a_move20162017.bust_a_move_framework.playermodel;
+package nl.tudelft.bust_a_move20162017.bust_a_move_framework.player;
 
 /**
  * The PlayerModel class represents a player entity.
