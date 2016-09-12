cac3b53f7a21a154aa21ad72a694fd9171641cbe
Winer Bao <winer-bao@hotmail.com>
2016-09-12 15:02:52 +0200

    LinkedList of Array storage implementation
    In progress; not finished yet.

--- bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/arena/ArenaModel.java
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/arena/ArenaModel.java
@@ -5,19 +5,20 @@
  * 
  * Version: 0.0.1
  * 
- * Date: September 7th 2016
+ * Date: September 12th 2016
  * 
  */
 
 
 package nl.tudelft.bust_a_move20162017.bust_a_move_framework.arena;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
-import nl.tudelft.bust_a_move20162017.bust_a_move_framework.graph.*;
+
 import nl.tudelft.bust_a_move20162017.bust_a_move_framework.bubble.*;
 
 /**
- * The arena class maintains a graph data structure to store the bubble objects. 
+ * The arena class maintains a LinkedList of ArrayList data structure to store the bubble objects. 
  *  
  * @author Winer Bao
  *
@@ -28,34 +29,42 @@ public class ArenaModel {
 	private int yPos;
 	private int height;
 	private int width;
-	private final Graph<BubbleModel, Integer> graph;
-	private LinkedList<BubbleModel> topBubbleList; 
+	private LinkedList<BubbleModel[]> bubble2DArray; 
 	
-	/* Amount of bubbles that fit in the horizontal axis */
-	private final int width_hex;		
+	/* Max amount of bubbles that fit in the horizontal axis */
+	private final int WIDTH_BUBBLES = 8;		
 	
-	/* Amount of bubbles that fit in the vertical axis */
-	private final int height_hex;	
+	/* Max amount of bubbles that fit in the vertical axis */
+	private final int HEIGHT_BUBBLES = 12;	
 	
 	/* Keep track how many bubbles are shot */
 	private int bubbleCount;
 	
+	/* Temporarily Bubble diameter variable */
+	private final int DIAMETER = 40; 
+	
 	/**
 	 * Creates Arena for bubbles storage and render variables
+	 * 
+	 * @param xVal		X coordinate of the background
+	 * @param yVal		Y coordinate of the background
+	 * @param height_t	height of the background
+	 * @param width_t	width of the background
+	 * 
 	 */
 	public ArenaModel(int xVal, int yVal, int height_t, int width_t) {
+		
 		xPos = xVal;
 		yPos = yVal;
 		height = height_t;
 		width = width_t;
-		graph = new Graph<BubbleModel, Integer>(false);
-		topBubbleList = new LinkedList<BubbleModel>();
-		
-		/* TODO: calculate width_hex and height_hex */
-		width_hex = 8;
-		height_hex = 12;
-		
+		bubble2DArray = new LinkedList<BubbleModel[]>();
 		bubbleCount = 0;
+		
+		//Level 1
+		for(int i = 0; i < 5; i++) {
+			addBubbleRow();
+		}
 	}
 	
 	/**
@@ -66,6 +75,7 @@ public class ArenaModel {
 	 * 
 	 */
 	public void set_xPos(int xVal) {
+		
 		xPos = xVal;
 	}
 	
@@ -76,6 +86,7 @@ public class ArenaModel {
 	 * 
 	 */
 	public int get_xPos() {
+		
 		return xPos;
 	}
 	
@@ -87,6 +98,7 @@ public class ArenaModel {
 	 * 
 	 */
 	public void set_yPos(int yVal) {
+		
 		yPos = yVal;
 	}
 	
@@ -97,34 +109,54 @@ public class ArenaModel {
 	 * 
 	 */
 	public int get_yPos() {
+		
 		return yPos;
 	}
 	
 	/**
-	 * Returns the storage graph in which all the bubbles are stored.
+	 * Returns the storage 2D array in which all the bubbles are stored.
 	 * 
-	 * @return storageGraph
+	 * @return bubble2DArray
 	 */
-	public Graph<BubbleModel, Integer> get_Graph() {
-		return graph;
+	public LinkedList<BubbleModel[]> get_BubbleArray() {
+		
+		return bubble2DArray;
 	}
 	
 	/**
 	 * Calculates where the shot bubble lands on the arena. The location is 
 	 * saved in the graph. It also calls the pop bubbles method. 
 	 * 
-	 * @param bubble_vtx The bubble shot by the cannon
+	 * @param shotBubble The bubble shot by the cannon
 	 * 
 	 */
-	public void landBubble(Vertex<BubbleModel, Integer>[] bubble_vtx) {
-		Vertex<BubbleModel, Integer> shotBubble_vtx = graph.addVertex(bubble_vtx[0]);
+	public void landBubble(BubbleModel shotBubble) {
 		
-		for(int i = 1; i < bubble_vertex.length; i++) {
-			graph.addEdge(shotBubble_vtx, bubble_vtx[i]);
+		int OFFSET = DIAMETER / 2; 				// Temporarily  
+		int row = shotBubble.getY() / DIAMETER;
+		int column;
+		
+		if(bubble2DArray.size() <= (row + 1)) {
+			// Even bubbles in a row
+			if(bubble2DArray.get(row).length == WIDTH_BUBBLES) {
+				column = shotBubble.getX()/ DIAMETER;
+			} else { // Uneven bubbles in a row
+				column = (shotBubble.getX() + OFFSET) / DIAMETER;
+			}
+		} else {
+			if(bubble2DArray.get(row-1).length == WIDTH_BUBBLES) {
+				column = (shotBubble.getX() + OFFSET) / DIAMETER;
+			} else { 
+				column = shotBubble.getX()/ DIAMETER;
+			}
 		}
 		
-		popBubbles(shotBubble_vtx);
-
+		while(bubble2DArray.size() != row) {
+			if((bubble2DArray.peekLast() != null) && (bubble2DArray.peekLast().length == WIDTH_BUBBLES)){
+				bubble2DAra
+			}
+		}
+		
 		bubbleCount++;
 		if(bubbleCount > 10) {
 			bubbleCount = 1;
