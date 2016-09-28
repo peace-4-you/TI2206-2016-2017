4b1c49aa5d758e991c67374f172678533bc1aacf
Winer Bao <winer-bao@hotmail.com>
2016-09-28 15:34:47 +0200

    checkstyle fix
    Solved trailing spaces errors
    Solved tab errors

--- bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/gamestate/MainMenu.java
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/gamestate/MainMenu.java
@@ -22,125 +22,106 @@ import nl.tudelft.bust_a_move20162017.bust_a_move_framework.App;
 
 /**
  * Generates a MainMenu as a instance of GameState.
- * 
  * @author Jason Xie, Maurice Willemsen
  */
-
 public class MainMenu extends BasicGameState {
+    /**
+     * Play Button.
+     */
+    private Button play;
+    /**
+     * Quit Button.
+     */
+    private Button quit;
+    /**
+     * setName Button.
+     */
+    private Button setName;
+    /**
+     * Name Textfield.
+     */
+    private TextField namefield;
+    /**
+     * Name Text.
+     */
+    private Text nameText;
 
-	/**
-	 * Play Button.
-	 */
-	private Button play;
-	/**
-	 * Quit Button.
-	 */
-	private Button quit;
-	/**
-	 * setName Button.
-	 */
-	private Button setName;
-	/**
-	 * Name Textfield.
-	 */
-	private TextField namefield;
-	/**
-	 * Name Text.
-	 */
-	private Text nameText;
+    /**
+     * @return integer of BasicGameState number.
+     */
+    public final int getID() {
+        return GameState.MAIN_MENU;
+    }
 
-	/**
-	 * @return integer of BasicGameState number.
-	 */
+    /**
+     * Called when BasicGameState initializes.
+     * @param game the game container
+     * @param stateBasedGame the state based game
+     * @throws SlickException any type of slick exception
+     */
+    public final void init(final GameContainer game,
+        final StateBasedGame stateBasedGame) throws SlickException {
+        nameText = new Text("Player: " + App.getGame().player.getName(),
+            GameConfig.FIRST_LINE);
+        nameText.centerText(game);
+        play = new Button("Play", GameConfig.THIRD_LINE, GameConfig.WIDTH1,
+            GameConfig.HEIGHT);
+        play.centerButton(game);
+        quit = new Button("Quit", GameConfig.FOURTH_LINE, GameConfig.WIDTH1,
+            GameConfig.HEIGHT);
+        quit.centerButton(game);
+        namefield = new TextField(game, game.getDefaultFont(),
+            GameConfig.CENTRAL, GameConfig.SIXT_LINE, GameConfig.WIDTH3,
+            GameConfig.HEIGHT);
+        namefield.setText("Player1");
+        setName = new Button("Set Name", GameConfig.SEVENTH_LINE,
+            GameConfig.WIDTH2, GameConfig.HEIGHT);
+        setName.centerButton(game);
+    }
 
-	public final int getID() {
-		return GameState.MAIN_MENU;
-	}
+    /**
+     * Renders the BasicGameState.
+     * @param game the game container
+     * @param stateBasedGame the state based game
+     * @param graphics Graphics object
+     * @throws SlickException any type of slick exception
+     */
 
-	/**
-	 * Called when BasicGameState initializes.
-	 * 
-	 * @param game
-	 *            the game container
-	 * @param stateBasedGame
-	 *            the state based game
-	 * @throws SlickException
-	 *             any type of slick exception
-	 */
+    public final void render(final GameContainer game,
+        final StateBasedGame stateBasedGame, final Graphics graphics)
+        throws SlickException {
+        nameText.draw(graphics);
+        play.draw(graphics);
+        quit.draw(graphics);
+        setName.draw(graphics);
+        namefield.render(game, graphics);
+    }
 
-	public final void init(final GameContainer game,
-		final StateBasedGame stateBasedGame) throws SlickException {
-		nameText = new Text("Player: " + App.getGame().player.getName(),
-			GameConfig.FIRST_LINE);
-		nameText.centerText(game);
-		play = new Button("Play", GameConfig.THIRD_LINE, GameConfig.WIDTH1,
-			GameConfig.HEIGHT);
-		play.centerButton(game);
-		quit = new Button("Quit", GameConfig.FOURTH_LINE, GameConfig.WIDTH1,
-			GameConfig.HEIGHT);
-		quit.centerButton(game);
-		namefield = new TextField(game, game.getDefaultFont(),
-			GameConfig.CENTRAL, GameConfig.SIXT_LINE, GameConfig.WIDTH3,
-			GameConfig.HEIGHT);
-		namefield.setText("Player1");
-		setName = new Button("Set Name", GameConfig.SEVENTH_LINE,
-			GameConfig.WIDTH2, GameConfig.HEIGHT);
-		setName.centerButton(game);
-	}
+    /**
+     * Updates the BasicGameState.
+     * @param game the game container
+     * @param stateBasedGame the state based game
+     * @param i delta of time exceeded
+     * @throws SlickException any type of slick exception
+     */
 
-	/**
-	 * Renders the BasicGameState.
-	 * 
-	 * @param game
-	 *            the game container
-	 * @param stateBasedGame
-	 *            the state based game
-	 * @param graphics
-	 *            Graphics object
-	 * @throws SlickException
-	 *             any type of slick exception
-	 */
-
-	public final void render(final GameContainer game,
-		final StateBasedGame stateBasedGame, final Graphics graphics)
-		throws SlickException {
-		nameText.draw(graphics);
-		play.draw(graphics);
-		quit.draw(graphics);
-		setName.draw(graphics);
-		namefield.render(game, graphics);
-	}
-
-	/**
-	 * Updates the BasicGameState.
-	 * 
-	 * @param game
-	 *            the game container
-	 * @param stateBasedGame
-	 *            the state based game
-	 * @param i
-	 *            delta of time exceeded
-	 * @throws SlickException
-	 *             any type of slick exception
-	 */
-
-	public final void update(final GameContainer game,
-		final StateBasedGame stateBasedGame, final int i)
-		throws SlickException {
-		nameText.setText("Player: " + App.getGame().player.getName());
-		nameText.centerText(game);
-		Input input = game.getInput();
-		if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-			if (play.isInBounds(input)) {
-				stateBasedGame.enterState(GameState.GAME_ACTIVE,
-					new FadeOutTransition(), new FadeInTransition());
-			}
-			if (setName.isInBounds(input)) {
-				App.getGame().player.setName(this.namefield.getText());
-			}
-			if (quit.isInBounds(input)) {
-				game.exit();
-			}
-		}
-	}
+    public final void update(final GameContainer game,
+        final StateBasedGame stateBasedGame, final int i)
+        throws SlickException {
+        nameText.setText("Player: " + App.getGame().player.getName());
+        nameText.centerText(game);
+        Input input = game.getInput();
+        if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+            if (play.isInBounds(input)) {
+                stateBasedGame.enterState(GameState.GAME_ACTIVE,
+                    new FadeOutTransition(), new FadeInTransition());
+            }
+            if (setName.isInBounds(input)) {
+                App.getGame().player.setName(this.namefield.getText());
+            }
+            if (quit.isInBounds(input)) {
+                game.exit();
+            }
+        }
+    }
 }
