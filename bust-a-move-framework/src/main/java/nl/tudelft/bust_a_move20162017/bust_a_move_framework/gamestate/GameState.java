53c0cd556dc39fd050ad7ad6e176cad57e0cd25b
Winer Bao <winer-bao@hotmail.com>
2016-09-28 15:14:28 +0200

    Fix checkstyle errors
    Solved trailing spaces
    Solved tab errors

--- bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/gamestate/GameState.java
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/gamestate/GameState.java
@@ -10,33 +10,31 @@ package nl.tudelft.bust_a_move20162017.bust_a_move_framework.gamestate;
 
 /**
  * Contains static integervalues of all GameStates.
- * 
  * @author Jason Xie
  */
-
 public abstract class GameState {
-	/**
-	 * Integer number of game state.
-	 */
-	public static final int START_SCREEN = 1;
-	/**
-	 * Integer number of game state.
-	 */
-	public static final int MAIN_MENU = 2;
-	/**
-	 * Integer number of game state.
-	 */
-	public static final int GAME_ACTIVE = 3;
-	/**
-	 * Integer number of game state.
-	 */
-	public static final int PAUSE_SCREEN = 4;
-	/**
-	 * Integer number of game state.
-	 */
-	public static final int WIN_SCREEN = 5;
-	/**
-	 * Integer number of game state.
-	 */
-	public static final int DEFEAT_SCREEN = 6;
+    /**
+     * Integer number of game state.
+     */
+    public static final int START_SCREEN = 1;
+    /**
+     * Integer number of game state.
+     */
+    public static final int MAIN_MENU = 2;
+    /**
+     * Integer number of game state.
+     */
+    public static final int GAME_ACTIVE = 3;
+    /**
+     * Integer number of game state.
+     */
+    public static final int PAUSE_SCREEN = 4;
+    /**
+     * Integer number of game state.
+     */
+    public static final int WIN_SCREEN = 5;
+    /**
+     * Integer number of game state.
+     */
+    public static final int DEFEAT_SCREEN = 6;
 }
