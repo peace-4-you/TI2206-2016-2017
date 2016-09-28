e5429eea4a50ec33ec8836f222d488264f66fff1
Winer Bao <winer-bao@hotmail.com>
2016-09-28 17:49:43 +0200

    Checkstyle error fixes
    Added package-info
    Solve trailing spaces errors
    Solve tab errors
    Solved @param errors

--- bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/player/Score.java
+++ bust-a-move-framework/src/main/java/nl/tudelft/bust_a_move20162017/bust_a_move_framework/player/Score.java
@@ -21,125 +21,113 @@ import nl.tudelft.bust_a_move20162017.bust_a_move_framework.App;
 
 /**
  * The Game class represents a game entity.
- *
  * @author Maurice Willemsen
  */
-
 public class Score extends Observable {
-	
-	private int score;
+    /**
+     * Integer variable to store the score value.
+     */
+    private int score;
+    /**
+     * Default points a popped bubble is worth.
+     */
+    private static final int DEFAULT_POPPED_POINTS = 10;
+    /**
+     * Default points a dropped bubble is worth.
+     */
+    private static final int DEFAULT_DROPPED_POINTS = 20;
+    /**
+     * Constructor of the Score class.
+     */
+    public Score() {
+        App.getGame().log.log(this, "Score initialised");
+        this.score = 0;
+        this.updateObserver();
+    }
 
-	public Score() {
-		App.getGame().log.log(this,"Score initialised");
-		this.score = 0;
-		this.updateObserver();
-	}
+    /**
+     * sets the player's score.
+     * @param scoreVal integer value to set score to
+     */
+    public final void setScore(final int scoreVal) {
+        App.getGame().log.log(this, "Score Set to " + scoreVal);
+        this.score = scoreVal;
+        this.updateObserver();
+    }
 
-	/**
-	 * sets the player's score
-	 *
-	 * @param score
-	 *            integer value to set score to
-	 *
-	 */
+    /**
+     * Sets the player's score to default values.
+     */
+    public final void reset() {
+        App.getGame().log.log(this, "Score Reset");
+        this.score = 0;
+        this.updateObserver();
+    }
+    /**
+     * Add to the player's score.
+     * @param scoreVal integer value to add to score
+     */
+    public final void addScore(final int scoreVal) {
+        App.getGame().log.log(this, "Score added with " + scoreVal);
+        this.score += scoreVal;
+        this.updateObserver();
+    }
+    /**
+     * Add popped bubble points to the player's score by algorithm.
+     * Each popped bubble is worth DEFAULT_POINTS points.
+     * @param bubbles number of bubbles popped
+     */
+    public final void scoreBubblesPopped(final int bubbles) {
+        if (bubbles > 0) {
+            int scoreVal = DEFAULT_POPPED_POINTS * bubbles;
+            App.getGame().log.log(this, bubbles + " Bubbles Popped.  "
+                    + "Score added with " + scoreVal);
+            this.score += scoreVal;
+            this.updateObserver();
+        }
+    }
+    /**
+     * Add dropped bubble points to the player's score by algorithm.
+     * Each dropped bubble is worth
+     * @param bubbles number of bubbles popped
+     */
+    public final void scoreBubblesDropped(final int bubbles) {
+        if (bubbles > 0) {
+            int scoreVal = (int) (DEFAULT_DROPPED_POINTS
+                    * Math.pow(2, bubbles));
+            App.getGame().log.log(this, bubbles + " bBubbles Dropped. "
+                    + "Score added with " + scoreVal);
+            this.score += scoreVal;
+            this.updateObserver();
+        }
+    }
 
-	public void setScore(int score) {
-		App.getGame().log.log(this,"Score Set to " + score);
-		this.score = score;
-		this.updateObserver();
-	}
-
-	/**
-	 * sets the player's score to default values
-	 *
-	 */
-
-	public void reset() {
-		App.getGame().log.log(this,"Score Reset");
-		this.score = 0;
-		this.updateObserver();
-	}
-	
-	/**
-	 * add to the player's score
-	 *
-	 * @param score
-	 *            integer value to add to score
-	 *
-	 */
-
-	public void addScore(int score) {
-		App.getGame().log.log(this,"Score added with " + score);
-		this.score += score;
-		this.updateObserver();
-	}
-
-	/**
-	 * add to the player's score by algorithm
-	 *
-	 * @param score
-	 *            integer value to add to score
-	 *
-	 */
-
-	public void scoreBubblesPopped(int bubbles) {
-		if (bubbles > 0) {
-			int score = 10 * bubbles;
-			App.getGame().log.log(this,bubbles + " Bubbles Popped.  Score added with " + score);
-			this.score += score;
-			this.updateObserver();
-		}
-	}
-
-	/**
-	 * add to the player's score by algorithm
-	 *
-	 * @param score
-	 *            integer value to add to score
-	 *
-	 */
-
-	public void scoreBubblesDropped(int bubbles) {
-		if (bubbles > 0) {
-			int score = (int) (20 * Math.pow(2, bubbles));
-			App.getGame().log.log(this,bubbles + " bBubbles Dropped. Score added with " + score);
-			this.score += score;
-			this.updateObserver();
-		}
-	}
-
-	/**
-	 * @return int score
-	 */
-
-	public int getScore() {
-		return this.score;
-	}
-	
-	/**
-	 * Updates connected observers
-	 */
-	
-	private void updateObserver(){
-		this.setChanged();	
-		this.notifyObservers(this.score);			
-	}
-	
-	/**
-	 * Adds class object to observer list
-	 * @param o Class Object
-	 */
-	
-	public void addAsObserver(Object o){
-		this.addObserver((Observer) o);
-	}
-	
-	/**
-	 * Deletes class object from observer list
-	 * @param o Class Object
-	 */
-	
-	public void deleteAsObser(Object o){
-		this.deleteObserver((Observer) o);
-	}
+    /**
+     * Getter method: returns the score.
+     * @return int score
+     */
+    public final int getScore() {
+        return this.score;
+    }
+    /**
+     * Updates connected observers.
+     */
+    private void updateObserver() {
+        this.setChanged();
+        this.notifyObservers(this.score);
+    }
+    /**
+     * Adds class object to observer list.
+     * @param o Class Object
+     */
+    public final void addAsObserver(final Object o) {
+        this.addObserver((Observer) o);
+    }
+    /**
+     * Deletes class object from observer list.
+     * @param o Class Object
+     */
+    public final void deleteAsObser(final Object o) {
+        this.deleteObserver((Observer) o);
+    }
 }
